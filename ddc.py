f7345e859d123fd2fcdf676190dd301881efa38c
stpapupa <149576507+stpapupa@users.noreply.github.com>
2023-12-02 20:08:27 +0700

    Add files via upload

---
+++ ddc.py
@@ -0,0 +1,7 @@
+hleb = int(input("хлеб"))
+moloko = int(input("молоко"))
+sosiski = int(input("сосисок"))
+h = 32
+s = 6
+m = 100
+print("дорогой хлеб-", hleb * h ,"очень дорогое молоко-", moloko * m ,"мега дешовые сосиски-", sosiski * s )
