f2371d0329b6cbeb4bcb8cb95c1b4dbf15f79ba3
stpapupa <149576507+stpapupa@users.noreply.github.com>
2024-01-10 13:23:27 +0300

    Add files via upload

---
+++ aee.py
@@ -0,0 +1,6 @@
+a=int(input("сколько кг ваш багаз"))
+if a<10 :
+    print("prohoditi mraz bezdushnaya")
+else:
+    print("tyazelo sliskom...." )
+print("cherez chas samolet letit v ykrainy")
---
+++ de.py
@@ -0,0 +1,6 @@
+a=int(input("ya leniviy"))
+b=int(input("ya leniviy2"))
+if a>b :
+    print("raznost ravna",a-b )
+else:
+    print("raznost ravna",b-a )
