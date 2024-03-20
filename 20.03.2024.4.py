54522baa5904c6b8b0320820ed101a6cb3aff1b7
stpapupa <149576507+stpapupa@users.noreply.github.com>
2024-03-20 14:23:07 +0300

    Add files via upload

---
+++ 20.03.2024.4.py
@@ -0,0 +1,30 @@
+print("Есть команды:raskaji_anekdot/top_film/top_game/top_anime")
+a=1
+stop = input("Введите stop ")
+while stop != "stop":
+    raskaji_anekdot = input("Введите raskaji_anekdot ")
+    while raskaji_anekdot != "raskaji_anekdot":
+        print("КОД НЕВЕРНЫЙ")
+        raskaji_anekdot = input("Введите raskaji_anekdot ")
+    print("русалка на шпагат села треш")
+    top_film = input("Введите top_film ")
+    while top_film != "top_film":
+        print("КОД НЕВЕРНЫЙ")
+        top_film = input("Введите top_film ")
+    print("эта фарфоровая кукла")
+    top_game = input("Введите top_game ")
+    while top_game != "top_game":
+        print("КОД НЕВЕРНЫЙ")
+        top_game = input("Введите top_game ")
+    print("brawl stars ороший игра кто не согласен тому делать джага-джага скибиди па-па")
+    top_anime = input("Введите top_anime ")
+    while top_anime != "top_anime":
+        print("КОД НЕВЕРНЫЙ")
+        top_anime = input("Введите top_anime ")
+    print("Хоримия")
+    stop = input("Введите stop ")
+    while a!= 1:
+        a=a+1
+        print(a)
+    
+        
